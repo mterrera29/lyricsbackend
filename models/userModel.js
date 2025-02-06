a9ef7d0fb2ef56c2a5f7c202743cfc6965ed9eb8
mterrera29 <matiasterrera29@gmail.com>
2025-02-06 00:59:35 -0300

    first commit

--- models/userModel.js
+++ models/userModel.js
@@ -13,11 +13,18 @@ const songSchema = new mongoose.Schema({
   scrollSpeedChords: Number,
 });
 
+const listSchema = new mongoose.Schema({
+  id: String,
+  name: String,
+  songIds: { type: Array, default: [] }
+});
+
 const userSchema = new mongoose.Schema({
   _id: String, // UID de Firebase como ID
   email: String,
   name: String,
   songs: [songSchema],
+  lists: [listSchema], 
 });
 
 const User = mongoose.model("User", userSchema);
